dcf40e40876a95f42c2998ae2ebb12f011007369
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-15 08:27:51 +0100

    Routing with NextJS

---
+++ app/categories/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+    params: {
+        slug: string
+    }
+}
+
+export default function CategoriesPage({ params }: Props) {
+  const { slug } = params;
+  return (
+    <div>CategoriesPage {slug}</div>
+  )
+}
